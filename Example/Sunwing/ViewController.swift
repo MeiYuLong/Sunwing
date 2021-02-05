4bb47abc16db005e933c719d40a122bbc1990652
YuLong Mei <longjiao914@126.com>
2021-02-05 11:18:57 +0800

    Initial commit

---
+++ Example/Sunwing/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Sunwing
+//
+//  Created by longjiao914@126.com on 02/05/2021.
+//  Copyright (c) 2021 longjiao914@126.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
