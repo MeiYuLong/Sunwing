65a8dd26ca784d1212b6306767e9806b9604423e
YuLong Mei <longjiao914@126.com>
2021-02-05 11:35:50 +0800

    Sunwing first commit

--- Example/Sunwing/ViewController.swift
+++ Example/Sunwing/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Sunwing
 
 class ViewController: UIViewController {
 
---
+++ Sunwing/Classes/SWClass.swift
@@ -0,0 +1,14 @@
+//
+//  SWClass.swift
+//  Sunwing
+//
+//  Created by yulong mei on 2021/2/5.
+//
+
+import Foundation
+
+struct SWCore {
+    static func startCore() {
+        print("startCore")
+    }
+}
